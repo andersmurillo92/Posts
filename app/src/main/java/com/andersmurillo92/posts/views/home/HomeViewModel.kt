d8d35d8a427ffd9f2b18841df31bad6ed8eb70cf
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2025-04-06 20:51:55 -0500

    Implement ViewModel for Home

---
+++ app/src/main/java/com/andersmurillo92/posts/views/home/HomeViewModel.kt
@@ -0,0 +1,51 @@
+package com.andersmurillo92.posts.views.home
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.andersmurillo92.posts.data.model.ResponseModel
+import com.andersmurillo92.posts.domain.PostsUseCase
+import com.andersmurillo92.posts.views.base.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val postsUseCase: PostsUseCase): ViewModel() {
+
+    companion object {
+        private const val TAG = "HomeViewModel"
+    }
+
+    val isLoading = MutableLiveData<Boolean>()
+    val posts = MutableLiveData<ResponseModel?>()
+
+    var singleLiveEvent: SingleLiveEvent<ViewEvent> = SingleLiveEvent()
+
+    fun onCreate(){
+        getPosts()
+    }
+
+    private fun getPosts(){
+        viewModelScope.launch {
+            try {
+                isLoading.value = true
+
+                val result: ResponseModel? = postsUseCase()
+                posts.value = result
+
+                isLoading.value = false
+                singleLiveEvent.value = ViewEvent.Success()
+            } catch (e: Exception){
+                Log.e(TAG, "Exception $e")
+                singleLiveEvent.value = ViewEvent.Error(e.message.toString())
+            }
+        }
+    }
+
+    sealed class ViewEvent {
+        class Success: ViewEvent()
+        class Error(val error: String?): ViewEvent()
+    }
+}
