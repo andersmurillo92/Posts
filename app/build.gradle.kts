514a21a473445c23f67b07b3b36ff96851cfdad6
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2025-04-06 20:45:42 -0500

    Add needed dependencies in gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,23 @@ dependencies {
     // Dagger hilt
     implementation("com.google.dagger:hilt-android:2.41")
     kapt("com.google.dagger:hilt-android-compiler:2.41")
+
+    // Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.6.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.6.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.9")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
 }
